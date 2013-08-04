e0a9d5e61e1f9c29aad4fd88a29cf8ec79679a3f
Ömer Başar <omerbasar@gmail.com>
2013-08-04 11:19:44 +0300

    unutulan küçük commitler

--- src/main/java/ob/cochelper/VillageHelper.java
+++ src/main/java/ob/cochelper/VillageHelper.java
@@ -26,12 +26,12 @@ public class VillageHelper {
 
       Map<Integer, Integer> wallMap = new TreeMap<Integer, Integer>();
 
-      String cannonCS = "11,11,10,10,10";
-      String archersCS = "10,10,9,9,9,9";
+      String cannonCS = "11,11,11,10,10";
+      String archersCS = "10,10,10,10,10,10";
       String mortars = "7,7,7";
       String wizardTowers = "6,6,6,6";
       String airDefenses = "7,6,6,6";
-      String hiddenTeslas = "7,7,6,5";
+      String hiddenTeslas = "7,7,6,6";
       String xBows = "3,2";
       String infernoTowers = "0";
 
@@ -44,18 +44,81 @@ public class VillageHelper {
       String darkElixirStorages = "6";
       int builders = 5;
       String armyCamps = "7,7,7,7";
-      String barracks = "10,10,9,8";
-      String darkBarracks = "4,3";
+      String barracks = "10,10,10,10";
+      String darkBarracks = "4,4";
       int laboratory = 7;
       int spellFactory = 4;
       int clanCastle = 4;
-      int barbarKing = 11;
-      int archerQuenn = 3;
+      int barbarKing = 13;
+      int archerQuenn = 5;
       int townHall = 9;
 
-      String spellLevels = "5,3,3,1,1";
-      String elixirTroopLevels = "6,6,5,5,5,3,5,1,3,3";
-      String darkElixirTroopLevels = "1,1,1,0";
+      String spellLevels = "5,4,3,1,1";
+      String elixirTroopLevels = "6,6,5,5,5,5,5,2,3,3";
+      String darkElixirTroopLevels = "1,1,1,1";
+
+      wallMap.put(5, 14);
+      wallMap.put(6, 166);
+      wallMap.put(7, 68);
+      wallMap.put(8, 2);
+
+      return new Village(townHall, cannonCS, archersCS, mortars, wizardTowers, airDefenses, hiddenTeslas, xBows, infernoTowers,
+              goldMines, elixirCollectors, darkElixirDrills,
+              goldStorages, elixirStorages, darkElixirStorages, builders, armyCamps, barracks, darkBarracks, laboratory, spellFactory,
+              clanCastle, barbarKing, archerQuenn, categories, upgradeCategories, wallMap, spellLevels, elixirTroopLevels, darkElixirTroopLevels,
+              untilTownHallLevel, untilLaboratoryLevel
+              );
+   }
+
+   public static Village getVillageOfYagmur() {
+
+      int untilTownHallLevel = 9;
+      int untilLaboratoryLevel = 7;
+
+      Set<BuildingCategory> categories = new HashSet<BuildingCategory>();
+
+      for (BuildingCategory buildingCategory : BuildingCategory.values()) {
+         if(buildingCategory.equals(BuildingCategory.WALL)){
+            continue;
+         }
+         categories.add(buildingCategory);
+      }
+
+      Set<UpgradeCategory> upgradeCategories = new HashSet<UpgradeCategory>();
+      Collections.addAll(upgradeCategories, UpgradeCategory.values());
+
+      Map<Integer, Integer> wallMap = new TreeMap<Integer, Integer>();
+
+      String cannonCS = "10,10,10,10,10";
+      String archersCS = "10,10,10,9,9,9";
+      String mortars = "6,6,6";
+      String wizardTowers = "6,6,5,5";
+      String airDefenses = "6,6,5,5";
+      String hiddenTeslas = "5,5,5,5";
+      String xBows = "1,1";
+      String infernoTowers = "0";
+
+      String goldMines = "11,11,11,11,11,11";
+      String elixirCollectors = "11,11,11,11,11,11";
+      String darkElixirDrills = "3,3";
+
+      String goldStorages = "11,11,11,11";
+      String elixirStorages = "11,11,11,11";
+      String darkElixirStorages = "4";
+      int builders = 5;
+      String armyCamps = "7,7,7,7";
+      String barracks = "10,8,8,8";
+      String darkBarracks = "2,2";
+      int laboratory = 7;
+      int spellFactory = 4;
+      int clanCastle = 4;
+      int barbarKing = 6;
+      int archerQuenn = 1;
+      int townHall = 9;
+
+      String spellLevels = "4,3,2,1,1";
+      String elixirTroopLevels = "6,6,5,4,5,4,3,1,3,1";
+      String darkElixirTroopLevels = "2,1,1,1";
 
       wallMap.put(5, 13);
       wallMap.put(6, 167);
@@ -91,10 +154,10 @@ public class VillageHelper {
       System.out.println("\n-----  total -------\n");
       village.getTotalStat().print();
 
-      System.out.println("\n--- remaining levels -----\n");
+      System.out.println("\n--- remaining levels -----");
       village.printRemainingLevels();
 
-      System.out.println("\n--- remaining upgrades -----\n");
+      System.out.println("\n--- remaining upgrades -----");
       village.printRemainingUpgradeLevels();
 
    }
